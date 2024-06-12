0693944ed6091559d1d5c774d7cb5c3c03a738b6
paddy meade <patrickmeade4@gmail.com>
2024-06-12 05:46:01 +0100

    display computer choice

--- script.js
+++ script.js
@@ -69,3 +69,31 @@ function computerRandomChoice() {
     computerChoice = "spock";
   }
 }
+
+// Add 'selected' styling & computerChoice
+function displayComputerChoice() {
+  switch (computerChoice) {
+    case "rock":
+      computerRock.classList.add("selected");
+      computerChoiceEl.textContent = " --- Rock";
+      break;
+    case "paper":
+      computerPaper.classList.add("selected");
+      computerChoiceEl.textContent = " --- Paper";
+      break;
+    case "scissors":
+      computerScissors.classList.add("selected");
+      computerChoiceEl.textContent = " --- Scissors";
+      break;
+    case "lizard":
+      computerLizard.classList.add("selected");
+      computerChoiceEl.textContent = " --- Lizard";
+      break;
+    case "spock":
+      computerSpock.classList.add("selected");
+      computerChoiceEl.textContent = " --- Spock";
+      break;
+    default:
+      break;
+  }
+}
